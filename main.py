ae26dbaa224d4ae13a2557b9d8845a4572467da5
Joaquín Rohland <62490781+soren-hub@users.noreply.github.com>
2023-02-03 00:13:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from flask import Flask, jsonify
-import os
+from products import products
 
-app = Flask(__name__)
+app = Flask(__name__) #servidor
 
+#crear rutas
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/ping', methods =['GET'])
+def ping():
+    return jsonify({"message": "pong"})
+
+#otra ruta
+#SE USA METHODS SEGUN LA ACCION QUE QUERAMOS SOBRE EL SERVIDOR:
+#GET VIENE POR DEFAULT
+#POST: PARA GUARDAR DATOS EN EL SERVIDOR
+#PUT: ACTUALIZAR DATOS
+#DELETE: BORRAR DATOS
+
+@app.route('/products', methods =['GET'])
+def getProducts():
+    return jsonify({"products": products, "message": "product's list"})
+
+#creacion de errores:
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        return jsonify({"product": productsFound[0]})
+    return jsonify({"message": "product not found"})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=4000)
